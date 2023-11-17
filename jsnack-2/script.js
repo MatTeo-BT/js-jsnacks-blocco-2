4e34be8d99772f609f0fda897720261944f62c32
MatTeo-BT <aboutmet@gmail.com>
2023-11-17 12:07:24 +0100

    impostazione esercizio

---
+++ jsnack-2/script.js
@@ -0,0 +1,8 @@
+const names = [`Franco`, `Peppe`, `Aldo`,`Giovanni`, `Giacomo`];
+const surnames = [`Baglio`, `Storti`, `Poretti`, `Moretti`,`Franchetti`];
+let i = 6;
+while(i < 6){
+    i++
+    names += surnames;
+}
+console.log(names, surnames, [i])
