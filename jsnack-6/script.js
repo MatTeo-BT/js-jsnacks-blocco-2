913b50de08fec7b74c9bd9c217bd3b775aa38f66
MatTeo-BT <aboutmet@gmail.com>
2023-11-21 12:18:32 +0100

    creato elemento

---
+++ jsnack-6/script.js
@@ -0,0 +1,6 @@
+const userNumber = prompt(`Type a number to generate squares`);
+
+function squareNumber (square){
+    const newArticle = document.createElement(`article`)
+    document.querySelector(`section.output`).appendChild(newArticle)
+}
